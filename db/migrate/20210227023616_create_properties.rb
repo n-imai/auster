390de924ba6e3b65c2141fd5e9f1a25025f50ecf
Nobuo Imai <8196611+n-imai@users.noreply.github.com>
2021-02-27 11:50:38 +0900

    create Properties by scaffold

---
+++ db/migrate/20210227023616_create_properties.rb
@@ -0,0 +1,9 @@
+class CreateProperties < ActiveRecord::Migration[6.1]
+  def change
+    create_table :properties do |t|
+      t.string :name
+      t.string :address
+      t.timestamps
+    end
+  end
+end
